2ffbeb0e51b34a10232b4d18f58103b6fb065c61
tzihklearn <1922941549@qq.com>
2025-08-15 22:04:47 +0800

    feat: 0815每日一题LT324

---
+++ src/main/java/com/tzih/learn/daily/LT324.java
@@ -0,0 +1,43 @@
+package com.tzih.learn.daily;
+
+/**
+ * @author tzih
+ * @version v1.0
+ * @since 2025.08.15
+ */
+public class LT324 {
+    /**
+     342. 4的幂
+     简单
+
+     给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
+     整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4x
+
+     示例 1：
+     输入：n = 16
+     输出：true
+
+     示例 2：
+     输入：n = 5
+     输出：false
+
+     示例 3：
+     输入：n = 1
+     输出：true
+
+     提示：
+     -231 <= n <= 231 - 1
+
+     进阶：你能不使用循环或者递归来完成本题吗？
+     */
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+        return n == 1;
+        // return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1;
+    }
+}
