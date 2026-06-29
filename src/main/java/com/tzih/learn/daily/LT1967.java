f49bd67480274d955ee1876bbb49c8cf770c0d43
tzih <1922941549@qq.com>
2026-06-29 23:31:29 +0800

    feat: 260629每日一题LT1967

---
+++ src/main/java/com/tzih/learn/daily/LT1967.java
@@ -0,0 +1,13 @@
+package com.tzih.learn.daily;
+
+public class LT1967 {
+    public int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+        for (String pattern : patterns) {
+            if (word.contains(pattern)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
